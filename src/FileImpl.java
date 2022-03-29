5e36a3104a296e11badbf26f2b15ad186034817e
jacksonlandry <jackson.landry@ontariotechu.net>
2022-03-29 14:23:30 -0400

    otp changes

--- src/FileImpl.java
+++ src/FileImpl.java
@@ -3,6 +3,7 @@ import java.rmi.*;
 import java.util.*;
 import java.rmi.server.UnicastRemoteObject;
 
+
 public class FileImpl extends UnicastRemoteObject
         implements FileInterface {
 
@@ -23,43 +24,6 @@ public class FileImpl extends UnicastRemoteObject
     public FileImpl(String s) throws RemoteException{
         super();
         name = s;
-    }
-
-    public byte[] downloadFile(String fileName, int clientID){
-        try {
-            array[i] = clientID;
-            File file = new File(fileName);
-            byte buffer[] = new byte[(int)file.length()];
-            BufferedInputStream input = new BufferedInputStream(new FileInputStream(fileName));
-            input.read(buffer,0,buffer.length);
-            System.out.println("The client's IP is: " + getClientHost());
-            System.out.println("The requested file is: " + fileName);
-            String history = myDownloadHistory(clientID, fileName);
-            System.out.println("Client ID: " + clientID + " downloaded: " + history);
-            System.out.println("ID: " + array[i]);
-            i = i + 1;
-            input.close();
-            return(buffer);
-        } catch(Exception e){
-            System.out.println("FileImpl: "+e.getMessage());
-            e.printStackTrace();
-            return(null);
-        }
-    }
-
-    public String myDownloadHistory(int clientID, String fileName) throws RemoteException{
-        id = clientID;
-        file = ", " + fileName;
-
-        if(keys.get(id) == null) {
-            keys.put(clientID, fileName);
-        }
-        else{
-            keys.put(clientID, keys.get(clientID) + file);
-        }
-
-        return(keys.get(clientID));
-
     }
 
     public boolean checkUser(int y) throws IOException{
@@ -86,114 +50,89 @@ public class FileImpl extends UnicastRemoteObject
 
     }
 
-    public void createUser(int y) throws IOException {
-        //Creates new user and adds to database if the user did not exist already
-        String newUser = Integer.toString(y);
-        output = new BufferedWriter(new FileWriter("Users.txt", true));
-        ((BufferedWriter) output).newLine();
-        output.write(newUser);
-        output.close();
 
-        //creates new file to store user information
-        String fileName = newUser+".txt";
-        File userFile = new File(fileName);
-        userFile.createNewFile();
-        output1 = new BufferedWriter(new FileWriter(fileName, true));
-        output1.write(newAcc);
-        output1.close();
+    public String inbox(int user) throws IOException {
+        String currentUser = Integer.toString(user);
+        String fileNames = "";
+        File folder = new File("UserFiles/"+currentUser + "/inbox");
+        File[] listOfFiles = folder.listFiles();
+        
+        for (int i = 0; i < listOfFiles.length; i++) {
+          if (listOfFiles[i].isFile()) {
+            fileNames += "\n" + listOfFiles[i].getName();
+          } else if (listOfFiles[i].isDirectory()) {
+            System.out.println("Directory " + listOfFiles[i].getName());
+          }
+        }
 
-        /*y is the users account name, will also need to get user choice here for what they want to do next
-        int z = br.readInt();
-        if(z==3) {
-            showAccount(y);
-        }
-        else if(z==2){
-            //withdraw
-            int wd = br.readInt();
-            withdraw(y, wd);
-        }
-        else if(z==1){
-            //deposit
-            int dp = br.readInt();
-            deposit(y, dp);
-        }*/
+        return fileNames;
     }
 
-    public void showAccount(int x) throws IOException {
-        String newUser = Integer.toString(x);
-        String fileName = newUser+".txt";
+    public String read(int user, String email) throws IOException {
+        System.out.println(email);
+        String currentUser = Integer.toString(user);
+        String emailText = "";
+        String fileName = "UserFiles/"+currentUser + "/inbox/" + email;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                emailText += "\n" + line;
+            }
+         }
 
-        //view account
-        read = new BufferedReader(new FileReader(fileName));
-        accountInfo = "";
-        String line = read.readLine();
-        while(line != null){
-            accountInfo = accountInfo + (newLine + line);
-            line = read.readLine();
-        }
-        read.close();
-        System.out.println(accountInfo);
+        return emailText;
     }
 
-    public void withdraw(int x, int wd) throws IOException {
-        System.out.println("acount: " +x +"\nwithdraw amount: " + wd);
-        //substring(9) to remove balance: from first line of file to get current balance
-        String newUser = Integer.toString(x);
-        String fileName = newUser+".txt";
+    
+    public String sendEmail(int recUser, String subject, String body) throws IOException {
+        File myObj = new File("UserFiles/"+recUser + "/inbox/"+subject+".txt");
+        myObj.createNewFile();
+        FileWriter myWriter = new FileWriter("UserFiles/"+recUser + "/inbox/"+subject+".txt");
+        myWriter.write(subject);
+        myWriter.write("\n\n" + body);
+        myWriter.close();
 
-        //view account
-        read = new BufferedReader(new FileReader(fileName));
-        accountInfo = "";
-        String line = read.readLine();
-        String firstLine = line;
-        firstLine = firstLine.substring(9);
-        int balance = Integer.parseInt(firstLine);
-        int newBalance = balance - wd;
-        line = "Balance: " + newBalance;
-        while(line != null){
-            accountInfo = accountInfo + (line + newLine);
-            line = read.readLine();
-        }
-        PrintWriter pw = new PrintWriter(fileName);
-        pw.close();
-        output1 = new BufferedWriter(new FileWriter(fileName, true));
-        output1.write(accountInfo);
-        output1.write("\nWithdraw: " + wd);
-        output1.close();
-        read.close();
+        return "email sent";
     }
 
-    public void deposit(int x, int dp) throws IOException {
-        System.out.println("acount: " + x + "\ndeposit amount: " + dp);
+    public int OTP(int message0, int message1) throws IOException{
+        int n = 187; //public
+        int e = 23; //public key
+        int d = 7; //private key
+        int[] ranMessages = {100, 200};
+        Random random = new Random();
 
-        String newUser = Integer.toString(x);
-        String fileName = newUser+".txt";
 
-        //view account
-        read = new BufferedReader(new FileReader(fileName));
-        accountInfo = "";
-        String line = read.readLine();
-        String firstLine = line;
-        firstLine = firstLine.substring(9);
-        int balance = Integer.parseInt(firstLine);
-        int newBalance = balance + dp;
-        line = "Balance: " + newBalance;
-        while(line != null){
-            accountInfo = accountInfo + (line + newLine);
-            line = read.readLine();
-        }
-        PrintWriter pw = new PrintWriter(fileName);
-        pw.close();
-        output1 = new BufferedWriter(new FileWriter(fileName, true));
-        output1.write(accountInfo);
-        output1.write("\nDeposit: " + dp);
-        output1.close();
-        read.close();
-    }
+        //get random messages
+        //choose random message 0 or 1 and generate key k and return to sender
+        //compute 2 keys and send to recipient
+        //recipient receives original messages and decrypts using the key k and can see the original message
 
-    //this should be used to transfer funds from 1 user to another. Need to authenticate that receiving user exists,
-    //need to add the transaction to the account history as type transfer, need to subtract amount transferred from balance.
-    public void eTransfer(int x, int sum, int recUser) throws IOException {
+        int choice = (int)Math.round( Math.random());
+        System.out.println("choice: " + choice);
 
+        int key = random.nextInt(10) + 1;
+        System.out.println("key: " + key);
+
+
+        int encryptedV = (int)((ranMessages[choice] + Math.pow(key, e)) % n);
+        System.out.println("encyrypted V: " + encryptedV);
+
+        int k0 = Math.abs((int)( Math.pow(encryptedV - ranMessages[0], d) % n));
+        int k1 = Math.abs((int)( Math.pow(encryptedV - ranMessages[1], d) % n));
+        System.out.println("k0: " + k0 + "\nk1: " + k1);
+
+        int hiddenMessage = message0 + k0;
+        int hiddenMessage2 = message1 + k1;
+        System.out.println("hidden messages: \n1. " + hiddenMessage + "\n2. " + hiddenMessage2);
+
+        int[] hiddenMessages = {hiddenMessage, hiddenMessage2};
+
+        int decryptedMessage = hiddenMessages[choice] - key;
+
+        System.out.print(decryptedMessage);
+
+        return decryptedMessage;
     }
 }
